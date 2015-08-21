682576453f563a8b32b1a1f9de31fd96ecc8efd2
Artsy <artsy@Artsys-MacBook-Air.local>
2015-08-21 00:58:35 -0400

    polish

--- build.js
+++ build.js
@@ -2,10 +2,11 @@ var markdown = require('markdown').markdown,
     fs = require('fs'),
     cheerio = require('cheerio'),
     jade = require('jade'),
-    request = require('superagent');
+    request = require('superagent'),
+    typeset = require('typeset');
 
 request
-  .get('https://raw.githubusercontent.com/artsy/ezel/gh-pages/README.md')
+  .get('https://raw.githubusercontent.com/artsy/ezel/master/README.md')
   .end(function(err, res) {
     if (err) {
       console.warn(err);
@@ -13,6 +14,11 @@ request
     }
     var $ = cheerio.load(markdown.toHTML(res.text));
     var navItems = $('h2').map(function(i, el) { return $(el).text() });
+    // $('p').each(function(i, el) {
+    //   var h = typeset($(el).html());
+    //   console.log(h)
+    //   $(el).html(h);
+    // })
 
     var html = jade.compile(fs.readFileSync('./index.jade'), {
       filename: __dirname + '/index.jade',
