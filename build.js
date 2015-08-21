7cd3e6b721a095c7bc2f5a0b787a2abdce74b9f2
Artsy <artsy@Artsys-MacBook-Air.local>
2015-08-21 00:05:05 -0400

    polish

--- build.js
+++ build.js
@@ -1,19 +1,30 @@
 var markdown = require('markdown').markdown,
     fs = require('fs'),
     cheerio = require('cheerio'),
-    jade = require('jade');
+    jade = require('jade'),
+    request = require('superagent');
 
-var mdHTML = markdown.toHTML(fs.readFileSync('./README.md').toString());
-var $ = cheerio.load(mdHTML);
-var navItems = $('h2').map(function(i, el) { return $(el).text() });
+request
+  .get('https://raw.githubusercontent.com/artsy/ezel/gh-pages/README.md')
+  .end(function(err, res) {
+    if (err) {
+      console.warn(err);
+      return process.exit(1);
+    }
+    var $ = cheerio.load(markdown.toHTML(res.text));
+    var navItems = $('h2').map(function(i, el) { return $(el).text() });
 
-var html = jade.compile(fs.readFileSync('./index.jade'), {
-  filename: __dirname + '/index.jade',
-})({ navItems: navItems });
-var $ = cheerio.load(html);
+    var html = jade.compile(fs.readFileSync('./index.jade'), {
+      filename: __dirname + '/index.jade',
+    })({
+      navItems: navItems,
+      body: $.html()
+    });
+    var $ = cheerio.load(html);
 
-$('h2').each(function() {
-  $(this).attr('id', $(this).text().toLowerCase());
-});
+    $('h2').each(function() {
+      $(this).attr('id', $(this).text().toLowerCase());
+    });
 
-fs.writeFileSync('./index.html', $.html());
+    fs.writeFileSync('./index.html', $.html());
+  });
